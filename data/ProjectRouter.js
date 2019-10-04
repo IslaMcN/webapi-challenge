a879ba4f816cb3b4b22f09f4b0eed21925622f20
IslaMcN <51002520+IslaMcN@users.noreply.github.com>
2019-10-04 09:51:46 -0700

    finished projects portion

--- data/ProjectRouter.js
+++ data/ProjectRouter.js
@@ -10,19 +10,37 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-
+    const project = req.body
+    db.insert(project)
+    .then(data => {
+        res.status(200).json({message: "Post successful"})
+    })
+    .catch(err => {
+        console.log('Nope', err)
+    })
 })
 
-router.post('/:id', (req, res) => {
-
-})
 
 router.put('/:id', (req, res) => {
-
+    const project = req.params.id
+    db.update(project, req.body)
+    .then(data => {
+        res.send(201).json({message:`Update: ${data} --- Update Successful`})
+    })
+    .catch(err => {
+        console.log('Nope', err)
+    })
 })
 
 router.delete('/:id', (req, res) => {
-
+    const project = req.params.id
+    db.remove(project)
+    .then(data => {
+        res.status(200).json(data)
+    })
+    .catch(err => {
+        console.log('Nope', err)
+    })
 })
 
 module.exports = router;
