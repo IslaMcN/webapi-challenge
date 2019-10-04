4f69c4c9417b8017edfc9b29bbddb945189dc476
IslaMcN <51002520+IslaMcN@users.noreply.github.com>
2019-10-04 10:39:43 -0700

    made it pretty

--- data/ProjectRouter.js
+++ data/ProjectRouter.js
@@ -1,49 +1,48 @@
-const express = require('express');
-const db = require('../data/helpers/projectModel');
+const express = require("express");
+const db = require("../data/helpers/projectModel");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    db.get()
+router.get("/", (req, res) => {
+  db.get()
     .then(data => {
-        res.status(200).json(data)
+      res.status(200).json(data);
     })
     .catch(err => {
-        console.log('Nope', err)
-    })
-})
+      console.log("Nope", err);
+    });
+});
 
-router.post('/', (req, res) => {
-    const project = req.body
-    db.insert(project)
+router.post("/", (req, res) => {
+  const project = req.body;
+  db.insert(project)
     .then(data => {
-        res.status(200).json({message: "Post successful"})
+      res.status(200).json({ message: "Post successful" });
     })
     .catch(err => {
-        console.log('Nope', err)
-    })
-})
+      console.log("Nope", err);
+    });
+});
 
-
-router.put('/:id', (req, res) => {
-    const project = req.params.id
-    db.update(project, req.body)
+router.put("/:id", (req, res) => {
+  const project = req.params.id;
+  db.update(project, req.body)
     .then(data => {
-        res.send(201).json({message:`Update: ${data} --- Update Successful`})
+      res.send(201).json({ message: `Update: ${data} --- Update Successful` });
     })
     .catch(err => {
-        console.log('Nope', err)
-    })
-})
+      console.log("Nope", err);
+    });
+});
 
-router.delete('/:id', (req, res) => {
-    const project = req.params.id
-    db.remove(project)
+router.delete("/:id", (req, res) => {
+  const project = req.params.id;
+  db.remove(project)
     .then(data => {
-        res.status(200).json(data)
+      res.status(200).json(data);
     })
     .catch(err => {
-        console.log('Nope', err)
-    })
-})
+      console.log("Nope", err);
+    });
+});
 
 module.exports = router;
